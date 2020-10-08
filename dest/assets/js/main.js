f85e809a6077d7820c9867b5186cbc0019584b1c
patwaryjowel <patwaryjowel52@gmail.com>
2020-10-08 23:05:42 +0600

    ES6 Module Stystem

---
+++ dest/assets/js/main.js
@@ -0,0 +1,34 @@
+
+
+/* ============== Module System Start =============== */
+
+
+
+import * as math from './plugins/math.js';
+
+console.log(math.add(20,30));
+
+import {add, dic} from './plugins/math.js';
+
+console.log(`${dic(50,30)} and ${add(40,30)} `);
+
+
+
+/* ============== Module System End =============== */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dest/assets/js/plugins/math.js
@@ -0,0 +1,2 @@
+export const add = (a,b) => a+b;
+export const dic = (a,b) => a-b;
